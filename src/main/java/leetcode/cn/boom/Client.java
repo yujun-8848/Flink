b342c3d6b863694c352d7316abf080dc808a4489
yujun-8848 <1505411476@qq.com>
2020-10-29 17:43:20 +0800

    update: bloomfilters

---
+++ src/main/java/leetcode/cn/boom/Client.java
@@ -0,0 +1,29 @@
+package leetcode.cn.boom;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author yujun
+ * @version 1.0.0
+ * @ClassName Client.java
+ * @Description TODO
+ * @createTime 2020年10月29日 17:11:00
+ */
+public class Client {
+
+    @Test
+    public void test(){
+        long star = System.currentTimeMillis();
+        BloomFilters bloomFilters = new BloomFilters(10000000);
+        for (int i = 0; i < 10000000; i++) {
+            bloomFilters.add(i + "");
+        }
+        Assert.assertTrue(bloomFilters.check(1+""));
+        Assert.assertTrue(bloomFilters.check(2+""));
+        Assert.assertFalse(bloomFilters.check(400230340
+                +""));
+        long end = System.currentTimeMillis();
+        System.out.println("执行时间:" + (end - star));
+    }
+}
